2c5db28703aed39ecdea5fc6cca0a48e37dc8026
DB-Conneely <danny.conneely13@gmail.com>
2025-09-30 22:07:00 +0100

    mobile hero text polish

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero: React.FC = () => {
   return (
     <section className="section" id="hero">
       <h1 className="glow">ABYSS GLOBAL</h1>
-      <p>Explore The Abyss</p>
+      <p>Explore The Abyss: My Evolving Portfolio</p>
     </section>
   );
 };
