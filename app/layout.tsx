697f77fa62b10ee48c1295155ede458c545dca55
DB-Conneely <danny.conneely13@gmail.com>
2025-10-01 05:04:55 +0100

    font size increase to avoid zoom on iphone

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> 
         <link rel="icon" href="/abyss-logo.png" />
         <link rel="apple-touch-icon" href="/abyss-logo.png" sizes="180x180" />
       </head>
